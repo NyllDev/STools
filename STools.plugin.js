c0f45f4a3797513fa5693d325e5aa922085b19e0
Денис Nyll <dev@nyll.dev>
2021-07-31 10:58:56 +0400

    test update 2

--- STools.plugin.js
+++ STools.plugin.js
@@ -2,7 +2,7 @@
  * @name STools
  * @author Nyll
  * @authorId 239559651203481600
- * @version 1.0.0
+ * @version 1.0.1
  * @description Кара божья.
  * @author Denis Solo (Windsor).
  * @website https://nyll.dev/
